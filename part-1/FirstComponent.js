042123032b42c66774d4626e29c178ad81426d7d
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-15 16:00:11 -0400

    Update FirstComponent.js

--- part-1/FirstComponent.js
+++ part-1/FirstComponent.js
@@ -2,5 +2,13 @@ function firstComponent() {
     return <h1>My very first component</h1>;
 }
 
+function App() {
+    return (
+      <div>
+        <FirstComponent />
+
+      </div>
+    );
+  }
 
 ReactDOM.render(<firstComponent/>, document.getElementById("root"))
